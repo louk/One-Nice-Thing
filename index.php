c1161ffbb4fad1e8f8bc524bf074e99038dee126
khangaikh <khangai@alchemist.mn>
2015-12-20 08:18:56 +0800

    PHP project initialized

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    require_once 'includes/Twig/Autoloader.php';
+    require_once "config.php";
+    use Parse\ParseObject;
+    use Parse\ParseClient;
+    use Parse\ParseQuery;
+    use Parse\ParseUser;
+    
+    session_start();
+    //register autoloader
+    Twig_Autoloader::register();
+    //loader for template files
+    $loader = new Twig_Loader_Filesystem('templates');
+    //twig instance
+    $twig = new Twig_Environment($loader, array(
+        'cache' => 'cache',
+    ));
+    //load template file
+    $twig->setCache(false);
+
+    $template = $twig->loadTemplate('main.html');
+    //render a template
+    echo $template->render(array('title' => 'Start'));
+?>
+
