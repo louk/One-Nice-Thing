e26ff49227dfbc3003cfaa1269ddb5b7114427f1
Khangai Khurelbaatar <khangai@alchemist.mn>
2016-01-30 18:56:42 +0800

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,14 +1,124 @@
-/*(function($){
-  $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$.fn.api.settings.api = {
+ 	'login user': 'login.php',
+ 	'register user': 'user.php',
+  	'edit user' : 'edit.php?id={$id}'
+};
 
-$('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
- */
+
+var login_validate = {
+    fields: {
+        name: {
+            identifier: 'username',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your name'
+                }
+            ]
+        },
+        password: {
+            identifier: 'password',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter a password'
+                }
+            ]
+        }
+    },
+    inline : true,
+    on     : 'blur'
+}
+
+var register_validate = {
+    fields: {
+        first: {
+            identifier: 'first',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your first name'
+                }
+            ]
+        },
+        last: {
+            identifier: 'last',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter your last name'
+                }
+            ]
+        },
+        password: {
+            identifier: 'password',
+            rules: [
+				{
+					type   : 'empty',
+					prompt : 'Please enter a password'
+				},
+				{
+					type   : 'minLength[6]',
+					prompt : 'Your password must be at least {ruleValue} characters'
+				}
+			]
+        },
+        email: {
+            identifier: 'email',
+            rules: [
+                {
+                    type   : 'empty',
+                    prompt : 'Please enter a email'
+                },
+                {
+                	type   : 'email',
+                	prompt : 'Please enter a valid e-mail' 
+                }
+            ]
+        }
+    },
+    inline : true,
+    on     : 'blur'
+}
+
+          /*  $("#login").submit(function( event ) {
+                event.preventDefault();
+                var i = $('.ui.checkbox').checkbox('is checked');
+                $.post("login.php", {
+                    email: $("#email").val(),
+                    pass: $("#password").val(),
+                    check: i
+                }, function (result) {
+                    if(result!=0){
+                        var url = "index.php?dashboard";
+                        window.location.replace(url);  
+                    }
+                });
+            });
+
+            $("#register").submit(function( event ) {
+                event.preventDefault();
+                var pass1 = $("#pass_new").val();
+                var pass2 = $("#pass_re").val();
+                if(pass1===pass2){
+                    $.post("user.php", {
+                        first: $("#first").val(),
+                        last: $("#last").val(),
+                        email: $("#email_new").val(),
+                        pass: $("#pass_new").val(),
+                        check: 'register'
+                    }, function (result) {
+                        if(result==11){
+                            var url = "index.php?dashboard";
+                            window.location.replace(url);  
+                        }else{
+                            alert(result);
+                        }
+                    }); 
+                }else{
+                    alert("Password mismatch!");
+                    return;
+                }
+            });*/
