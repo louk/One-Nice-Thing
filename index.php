76ffa74ba53f3ac2eb8cf843d9ee3b613e6ce71d
khangai <khangai@alchemist.mn>
2016-02-03 19:21:29 +0800

    SOme changes

--- index.php
+++ index.php
@@ -30,36 +30,39 @@ use Parse\ParseUser;
             $template = $twig->loadTemplate('main.html');
             //render a template
             echo $template->render(array('title' => 'See you agian'));
-        }if (isset($_GET['dashboard'])) {
+        }else if (isset($_GET['dashboard'])) {
             $template = $twig->loadTemplate('dashboard.html');
             echo $template->render(array('title' => 'Dashboard', 'user' => $user, 'nav' => 1));
-        }if (isset($_GET['reportnicething'])) {
+        }else if (isset($_GET['reportnicething'])) {
             $query = new ParseQuery("_User");
             $query->equalTo('status', 1);
             $users = $query->find();
             $template = $twig->loadTemplate('reportnicething.html');
             echo $template->render(array('title' => 'Report Nice Thing', 'users' =>$users, 'user' => $user, 'nav' => 2));
-        }if (isset($_GET['chat'])) {
+        }else if (isset($_GET['chat'])) {
             $template = $twig->loadTemplate('chat.html');
             echo $template->render(array('title' => 'Inbox', 'user' => $user, 'nav' => 3));
-        }if (isset($_GET['explore'])) {
+        }else if (isset($_GET['explore'])) {
             $template = $twig->loadTemplate('explore.html');
             echo $template->render(array('title' => 'Explore', 'user' => $user, 'nav' => 4));
-        }if (isset($_GET['nicethings'])) {
+        }else if (isset($_GET['nicethings'])) {
             $template = $twig->loadTemplate('my-things.html');
             echo $template->render(array('title' => 'My nice things', 'user' => $user, 'nav' => 5));
-        }if (isset($_GET['mymap'])) {
+        }else if (isset($_GET['mymap'])) {
             $template = $twig->loadTemplate('my-map.html');
             echo $template->render(array('title' => 'My map', 'user' => $user, 'nav' => 6));
-        }if (isset($_GET['tree'])) {
+        }else if (isset($_GET['tree'])) {
             $template = $twig->loadTemplate('my-tree.html');
             echo $template->render(array('title' => 'My tree', 'user' => $user, 'nav' => 7));
-        }if (isset($_GET['friends'])) {
+        }else if (isset($_GET['friends'])) {
             $template = $twig->loadTemplate('friends.html');
             echo $template->render(array('title' => 'My friends', 'user' => $user, 'nav' => 8));
-        }if (isset($_GET['settings'])) {
+        }else if (isset($_GET['settings'])) {
             $template = $twig->loadTemplate('settings.html');
             echo $template->render(array('title' => 'My settings', 'user' => $user));
+        }else{
+            $template = $twig->loadTemplate('dashboard.html');
+            echo $template->render(array('title' => 'Dashboard', 'user' => $user, 'nav' => 1));
         }
     } else {
         if (isset($_GET['login'])) {
